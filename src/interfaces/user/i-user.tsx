1619427e6a38a71b0fc8f9b61c270f5f94aeceb1
MrAdib <Mr.John.Adib@gmail.com>
2024-10-02 21:17:57 +0100

    add interface and structure of components

---
+++ src/interfaces/user/i-user.tsx
@@ -0,0 +1,19 @@
+import { UserRole } from "./type-user-role";
+import { UserStatus } from "./type-user-status";
+
+interface IUserBase {
+  firstName: string;
+  lastName: string;
+  email: string;
+  bio?: string;
+  role: UserRole;
+  status: UserStatus;
+}
+
+export interface IUserWithPassword extends IUserBase {
+  password: string;
+}
+
+export interface IUserWithoutPassword extends IUserBase {
+  password?: never;
+}
