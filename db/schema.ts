4490aeb7c6fb6d1f7fe1c8a78b05c13b85f5bb8b
MrAdib <Mr.John.Adib@gmail.com>
2024-10-04 03:56:00 +0000

    update readme

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,5 @@
-import { pgTable, serial, varchar, pgEnum } from 'drizzle-orm/pg-core'
+import { pgTable, serial, varchar, pgEnum, index } from 'drizzle-orm/pg-core'
 
-// Define PostgreSQL enums for role and status
 export const userRoleEnum = pgEnum('user_role', ['user', 'admin'])
 export const userStatusEnum = pgEnum('user_status', [
   'active',
@@ -9,12 +8,22 @@ export const userStatusEnum = pgEnum('user_status', [
   'blocked'
 ])
 
-// Define the users table schema
-export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
-  name: varchar('name', { length: 255 }).notNull(),
-  email: varchar('email', { length: 255 }).notNull(),
-  role: userRoleEnum('role').notNull(),
-  bio: varchar('bio', { length: 1024 }).default(''),
-  status: userStatusEnum('status').notNull()
-})
+export const users = pgTable(
+  'users',
+  {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 255 }).notNull(),
+    email: varchar('email', { length: 255 }).notNull(),
+    role: userRoleEnum('role').notNull(),
+    bio: varchar('bio', { length: 1024 }).default(''),
+    status: userStatusEnum('status').notNull()
+  },
+  (table) => {
+    return {
+      emailIdx: index('email_idx').on(table.email),
+      roleIdx: index('role_idx').on(table.role),
+      statusIdx: index('status_idx').on(table.status),
+      roleStatusIdx: index('role_status_idx').on(table.role, table.status)
+    }
+  }
+)
