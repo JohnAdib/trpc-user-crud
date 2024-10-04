40bfbe2b00c08c45ef39f1a20df98cb4bd4c9e4c
MrAdib <Mr.John.Adib@gmail.com>
2024-10-04 03:05:52 +0100

    try to run migration!

--- db/index.ts
+++ db/index.ts
@@ -2,7 +2,10 @@ import { drizzle } from 'drizzle-orm/node-postgres'
 import { Client } from 'pg'
 import * as schema from './schema'
 
-const connectionString = process.env.DATABASE_URL
+const defaultConnection =
+  'postgresql://ThisIsUser:ThisIsPassword@db:5432/trpc-user-crud'
+
+const connectionString = process.env.DATABASE_URL || defaultConnection
 console.log('connectionString', connectionString)
 
 const dbConnection = new Client({ connectionString })
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'drizzle-kit'
 import dotenv from 'dotenv'
+import { hostname } from 'os'
 
 dotenv.config()
+
+const defaultConnection =
+  'postgresql://ThisIsUser:ThisIsPassword@db:5432/trpc-user-crud'
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: './db/schema.ts',
-  out: './db/drizzle'
+  out: './db/drizzle',
+  dbCredentials: {
+    url: process.env.DATABASE_URL || defaultConnection
+  }
 })
